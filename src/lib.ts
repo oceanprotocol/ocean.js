6d98da883b98712414385bae696e14caf98bf84d
alexcos20 <alex.coseru@gmail.com>
2020-05-26 06:42:22 -0700

    fix compute merge

--- src/lib.ts
+++ src/lib.ts
@@ -13,7 +13,7 @@ export * from './ddo/DDO'
 export * from './ddo/MetaData'
 
 export { CreateProgressStep } from './ocean/Assets'
-export { ComputeJob, ComputeJobStatus } from './ocean/OceanCompute'
+export { ComputeJob, ComputeJobStatus } from './ocean/Compute'
 export { OrderProgressStep } from './ocean/utils/ServiceUtils'
 export {
     OceanPlatformTechStatus,
