fcd1b8718822db52d11635be882ff176fad9626f
Alex Coseru <alex.coseru@gmail.com>
2020-05-26 11:41:25 +0300

    Delete ValueType.ts

--- src/models/ValueType.ts
+++
@@ -1,8 +0,0 @@
-enum ValueType {
-    DID, // DID string e.g. 'did:op:xxx'
-    DIDRef, // hash of DID same as in parameter (bytes32 _did) in text 0x0123abc.. or 0123abc..
-    URL, // URL string e.g. 'http(s)://xx'
-    DDO // DDO string in JSON e.g. '{ "id": "did:op:xxx"...
-}
-
-export default ValueType
