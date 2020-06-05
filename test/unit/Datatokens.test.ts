1379f48ad0b73413876e18150594de091685b46a
arsenyjin <kinspotting@protonmail.com>
2020-06-05 11:59:27 +0200

    add dummy test

---
+++ test/unit/Datatokens.test.ts
@@ -0,0 +1,11 @@
+import { assert } from 'chai'
+
+describe('Datatokens', () => {
+
+    describe('#test()', () => {
+        it('should test', async () => {
+            const test = true
+            assert(test === true)
+        })
+    })
+})
