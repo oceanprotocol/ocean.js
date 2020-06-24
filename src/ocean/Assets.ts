96d946690d6f1f3e7ebf46bfb89002f60ba077dd
alexcos20 <alex.coseru@gmail.com>
2020-06-24 06:30:09 -0700

    fix lint

--- src/ocean/Assets.ts
+++ src/ocean/Assets.ts
@@ -68,7 +68,7 @@ export class Assets extends Instantiable {
         dtAddress?: string
     ): SubscribablePromise<CreateProgressStep, DDO> {
         this.logger.log('Creating asset')
-        return new SubscribablePromise(async observer => {
+        return new SubscribablePromise(async (observer) => {
             if (services.length === 0) {
                 this.logger.log('You have no services. Are you sure about this?')
             }
@@ -147,7 +147,7 @@ export class Assets extends Instantiable {
                     )
                     .reverse()
                     // Adding index
-                    .map(_ => ({
+                    .map((_) => ({
                         ..._,
                         index: indexCount++
                     })) as Service[]
