cf2072ecab4cf76786420c8beb58c374a3e55b86
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-06-26 20:32:08 +0200

    minor config fix

--- test/integration/config.ts
+++ test/integration/config.ts
@@ -8,11 +8,8 @@ const web3 = new Web3('http://127.0.0.1:8545')
 
 export default {
     metadataStoreUri: 'http://localhost:5000',
-    //gimetadataStoreUri: 'https://aquarius.marketplace.dev-ocean.com',
     providerUri: 'http://localhost:8030',
     nodeUri: `http://localhost:${process.env.ETH_PORT || 8545}`,
-    parityUri: 'http://localhost:9545',
-    secretStoreUri: 'http://localhost:12001',
     verbose: LogLevel.Error,
     web3Provider: web3
 } as Config
