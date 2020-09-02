0290f57bf4d35534e21f46abe432d7d21c6ff980
Matthias Kretschmann <m@kretschmann.io>
2020-09-02 16:23:01 +0200

    prototype auto-generating data token name

--- src/datatokens/Datatokens.ts
+++ src/datatokens/Datatokens.ts
@@ -4,6 +4,8 @@ import { AbiItem } from 'web3-utils/types'
 import defaultFactoryABI from '@oceanprotocol/contracts/artifacts/DTFactory.json'
 import defaultDatatokensABI from '@oceanprotocol/contracts/artifacts/DataTokenTemplate.json'
 
+import wordListDefault from '../data/words.json'
+
 /**
  * Provides a interface to DataTokens
  */
@@ -30,6 +32,32 @@ export class DataTokens {
     this.factoryABI = factoryABI || (defaultFactoryABI.abi as AbiItem[])
     this.datatokensABI = datatokensABI || (defaultDatatokensABI.abi as AbiItem[])
     this.web3 = web3
+  }
+
+  /**
+   * Generate new datatoken name & symbol from a word list
+   * @return {<{ name: String; symbol: String }>} datatoken name & symbol. Produces e.g. "Endemic Jellyfish Token" & "ENDJEL-145"
+   */
+  public generateDtName(wordList?: {
+    nouns: string[]
+    adjectives: string[]
+  }): { name: string; symbol: string } {
+    const list = wordList || wordListDefault
+    const random1 = Math.floor(Math.random() * list.adjectives.length)
+    const random2 = Math.floor(Math.random() * list.nouns.length)
+    const indexNumber = Math.floor(Math.random() * 100)
+
+    // Capitalized adjective & noun
+    const adjective = list.adjectives[random1].replace(/^\w/, (c) => c.toUpperCase())
+    const noun = list.nouns[random2].replace(/^\w/, (c) => c.toUpperCase())
+
+    const name = `${adjective} ${noun} Token`
+    // use first 3 letters of name, uppercase it, and add random number
+    const symbol = `${(
+      adjective.substring(0, 3) + noun.substring(0, 3)
+    ).toUpperCase()}-${indexNumber}`
+
+    return { name, symbol }
   }
 
   /**
