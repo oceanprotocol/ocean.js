a725b00038b74b817fca93efecba789bd713d9f9
Matthias Kretschmann <m@kretschmann.io>
2020-09-18 12:05:26 +0200

    test updates

--- test/unit/ocean/DID.test.ts
+++ test/unit/ocean/DID.test.ts
@@ -1,27 +1,26 @@
 import assert from 'assert'
 import DID from '../../../src/ocean/DID'
 
+const dataTokenMock = '0x0000000000000000000000000000000000000000'
+
 describe('DID', () => {
   describe('#generate()', () => {
     it('should generate a new did', () => {
-      const did: DID = DID.generate()
+      const did: DID = DID.generate(dataTokenMock)
       assert(did)
     })
   })
 
   describe('#parse()', () => {
     it('should parse a valid did', () => {
-      const id = 'a'.repeat(64)
-      const did: DID = DID.parse(`did:op:${id}`)
+      const did: DID = DID.parse(`did:op:${dataTokenMock}`)
       assert(did)
-
-      assert(did.getId() === id, did.getId())
+      assert(did.getDid())
     })
 
     it('should throw if prefix does not match', (done) => {
-      const id = '1234'
       try {
-        const did: DID = DID.parse(`did:xxx:${id}`)
+        const did: DID = DID.parse(`did:xxx:${dataTokenMock}`)
         assert(!did)
       } catch {
         done()
@@ -41,20 +40,10 @@ describe('DID', () => {
 
   describe('#getDid()', () => {
     it('should return the full did', () => {
-      const did: DID = DID.generate()
-      assert(did)
+      const did: DID = DID.generate(dataTokenMock)
 
+      assert(did)
       assert(did.getDid().startsWith('did:op:'))
-    })
-  })
-
-  describe('#getDid()', () => {
-    it('should return only the id part of the did', () => {
-      const id = 'a'.repeat(64)
-      const did: DID = DID.parse(`did:op:${id}`)
-      assert(did)
-
-      assert(did.getId() === id)
     })
   })
 })
