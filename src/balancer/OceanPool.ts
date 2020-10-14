da5e3dcc263667761c336dd1057b96b394ebdbc9
alexcos20 <alex.coseru@gmail.com>
2020-10-14 03:02:35 -0700

    remove unwanted dep

--- src/balancer/OceanPool.ts
+++ src/balancer/OceanPool.ts
@@ -4,8 +4,6 @@ import { TransactionReceipt } from 'web3-core'
 import { Pool } from './Pool'
 import { EventData, Filter } from 'web3-eth-contract'
 import BigNumber from 'bignumber.js'
-import Decimal from 'decimal.js'
-import { createDiffieHellman } from 'crypto'
 
 declare type PoolTransactionType = 'swap' | 'join' | 'exit'
 
