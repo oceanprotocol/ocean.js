181d260e0ac71aa90307b37fd2fd7618ce972603
mihaisc <mihai.scarlat@smartcontrol.ro>
2020-11-24 15:34:12 +0200

    Handle assets with low liquidity (#468)
    * Handle assets with low liquidity
    
    `isConsumable` is : 
    - 'true' when pool/exchange created and enough datatokens are available ( can we check for exchange?)
    - 'false' when pool/exchange created and not enough datatokens are avaialbe
    - '' when pool/exchange not created
    
    * isConsumable optional

--- src/ddo/interfaces/BestPrice.ts
+++ src/ddo/interfaces/BestPrice.ts
@@ -2,6 +2,7 @@ export interface BestPrice {
   type: 'pool' | 'exchange' | ''
   address: string
   value: number
+  isConsumable?: 'true' | 'false' | ''
   ocean?: number
   datatoken?: number
   pools: string[]
