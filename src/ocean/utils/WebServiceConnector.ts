6814d774ded4575055531f2256c02df2e027bd79
mihaisc <mihai.scarlat@smartcontrol.ro>
2021-01-12 19:16:23 +0200

    remove console
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- src/ocean/utils/WebServiceConnector.ts
+++ src/ocean/utils/WebServiceConnector.ts
@@ -68,83 +68,11 @@ export class WebServiceConnector {
     url: string,
     destination?: string,
     index?: number
-  ): Promise<string> {
-    // const response = await this.get(url)
-    // if (!response.ok) {
-    //   throw new Error('Response error.')
-    // }
-    // let filename: string
-    // try {
-    //   filename = response.headers
-    //     .get('content-disposition')
-    //     .match(/attachment;filename=(.+)/)[1]
-    // } catch {
-    //   try {
-    //     filename = url.split('/').pop()
-    //   } catch {
-    //     filename = `file${index}`
-    //   }
-    // }
-
-    // if (destination) {
-    //   // eslint-disable-next-line no-async-promise-executor
-    //   await new Promise(async (resolve, reject) => {
-    //     fs.mkdirSync(destination, { recursive: true })
-    //     const fileStream = fs.createWriteStream(`${destination}${filename}`)
-    //     response.body.pipe(fileStream)
-    //     response.body.on('error', reject)
-    //     fileStream.on('finish', resolve)
-    //   })
-
-    //   return destination
-    // } else {
-    //   save(await response.arrayBuffer(), filename)
-    // }
-    console.log('ocean lib 4')
-    console.log('dl ulr', url)
+  ): Promise<void> {
     const anchor = document.createElement('a')
     anchor.download = ''
     anchor.href = url
     anchor.click()
-    return ''
-    // const { createWriteStream } = await import('streamsaver')
-
-    // return fetch(url).then((res) => {
-    //   let filename: string
-    //   try {
-    //     filename = res.headers
-    //       .get('content-disposition')
-    //       .match(/attachment;filename=(.+)/)[1]
-    //   } catch {
-    //     try {
-    //       filename = url.split('/').pop()
-    //     } catch {
-    //       filename = `file${index}`
-    //     }
-    //   }
-    //   console.log('filename ', filename)
-    //   // create a writable stream + intercept a network response
-    //   const fileStream = createWriteStream(filename)
-    //   const writer = fileStream.getWriter()
-
-    //   // stream the response
-    //   const reader = res.body.getReader()
-    //   const pump = () =>
-    //     reader.read().then(({ value, done }) => {
-    //       // Write one chunk, then get the next one
-    //       writer.write(value) // returns a promise
-
-    //       // While the write stream can handle the watermark,
-    //       // read more data
-    //       return writer.ready.then(pump)
-    //     })
-
-    //   // Start the reader
-    //   pump().then(() => {
-    //     console.log('Closed the stream, Done writing')
-    //     writer.close()
-    //   })
-    // })
   }
 
   private async fetch(url: string, opts: RequestInit): Promise<Response> {
