6783419163afacb66bcc31358057626c3b68b621
Matthias Kretschmann <m@kretschmann.io>
2021-01-26 20:43:17 +0100

    refactor typedoc generation

--- scripts/typedoc.js
+++ scripts/typedoc.js
@@ -3,38 +3,18 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 
 const fs = require('fs')
-const TypeDoc = require('typedoc')
-const typescript = require('typescript')
 const ora = require('ora')
 const libJsPackage = require('../package.json')
+const { spawn } = require('child_process')
 
 const { description, version } = libJsPackage
 
 // Setup our paths, relative to project root
-const outPath = './dist/lib.json'
-const files = ['./src/lib.ts']
-
-// specifically point to tsconfig, otherwise TypeDoc fails
-const config = typescript.findConfigFile('./tsconfig.js', typescript.sys.fileExists)
+const outPath = './dist/ocean.js.json'
+const input = './src/lib.ts'
 
 const generateJson = async () => {
   const spinnerTypedoc = ora('Generating TypeDoc json...').start()
-
-  // Setup our TypeDoc app
-  const app = new TypeDoc.Application()
-  app.options.addReader(new TypeDoc.TSConfigReader())
-  app.options.addReader(new TypeDoc.TypeDocReader())
-
-  app.bootstrap({
-    tsconfig: config,
-    entryPoints: files
-  })
-
-  const src = app.expandInputFiles(files)
-  const project = app.convert(src)
-
-  // Generate the JSON file
-  await app.generateJson(project, outPath)
 
   // Parse and modify json output
   const jsonOrig = await JSON.parse(fs.readFileSync(outPath, 'utf8'))
@@ -54,4 +34,8 @@ const generateJson = async () => {
   spinnerTypedoc.succeed('Generated TypeDoc json.')
 }
 
-generateJson().catch(console.error)
+const child = spawn(`typedoc`, [input, `--json ${outPath}`])
+
+child.on('exit', () => {
+  generateJson().catch(console.error)
+})
