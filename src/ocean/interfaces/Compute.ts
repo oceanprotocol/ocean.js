39938ec9c0d56ea39f5dc15e57a50030afa7eab9
Alex Coseru <alex.coseru@gmail.com>
2021-04-13 18:34:34 +0300

    refactor compute (#731)
    WARNING! This is a breaking change!
    
    * Refactor compute functions to use a new interface (ComputeAlgorithm) instead of passing 4-5 params to different functions.
    * add new test to prevent running a computeJob is both asset/algo are having compute services and they are not served from the same provider
    
    Co-authored-by: Matthias Kretschmann <m@kretschmann.io>

---
+++ src/ocean/interfaces/Compute.ts
@@ -0,0 +1,43 @@
+import DID from '../DID'
+import { Metadata } from '../../ddo/interfaces/Metadata'
+import { MetadataAlgorithm } from '../../ddo/interfaces/MetadataAlgorithm'
+
+export interface ComputeJob {
+  owner: string
+  did: string
+  jobId: string
+  dateCreated: string
+  dateFinished: string
+  status: number
+  statusText: string
+  algorithmLogUrl: string
+  resultsUrl: string[]
+  resultsDid?: DID
+}
+
+export interface ComputeOutput {
+  publishAlgorithmLog?: boolean
+  publishOutput?: boolean
+  providerAddress?: string
+  providerUri?: string
+  metadata?: Metadata
+  metadataUri?: string
+  nodeUri?: string
+  owner?: string
+  secretStoreUri?: string
+  whitelist?: string[]
+}
+
+export interface ComputeInput {
+  documentId: string
+  serviceId: number
+  transferTxId?: string
+}
+
+export interface ComputeAlgorithm {
+  did?: string
+  serviceIndex?: number
+  meta?: MetadataAlgorithm
+  transferTxId?: string
+  dataToken?: string
+}
