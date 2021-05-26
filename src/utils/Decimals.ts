242bcdf333e51f388adf0465d035d5dc4c5e1d14
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-26 17:42:29 +0300

    adding default value to Decimals function

--- src/utils/Decimals.ts
+++ src/utils/Decimals.ts
@@ -1,5 +1,5 @@
 import Decimal from 'decimal.js'
-export default function reduceDecimals(number: string) {
-  Decimal.set({ toExpNeg: -18, precision: 18, rounding: 1 })
+export default function reduceDecimals(number: string, decimal = 18) {
+  Decimal.set({ toExpNeg: -decimal, precision: decimal, rounding: 1 })
   return String(new Decimal(number))
 }
