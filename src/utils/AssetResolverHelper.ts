200e0d8832de14a0fc63a2dc2518a3a295e417dc
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-06-08 09:08:51 +0300

    staged helper file

---
+++ src/utils/AssetResolverHelper.ts
@@ -0,0 +1,25 @@
+import { DDO } from '../ddo/DDO'
+import { Ocean } from '../ocean/Ocean'
+
+export interface AssetResolver {
+  did: string
+  ddo: DDO
+}
+
+function isDdo(arg: any): arg is DDO {
+  return arg.id !== undefined
+}
+
+export async function assetResolve(asset: DDO | string): Promise<AssetResolver> {
+  let ocean: Ocean
+
+  if (isDdo(asset)) {
+    const did = asset.id
+    const ddo = asset
+    return { did, ddo }
+  } else {
+    const ddo = await ocean.assets.resolve(asset)
+    const did = ddo.id
+    return { did, ddo }
+  }
+}
