5c62c97de141a9bfdc454c242f2152fde89c451b
TAZHENH <iforte_intellige.zhenhao@daimler.com>
2021-07-29 15:38:29 +0800

    Linting fix

--- src/ocean/Assets.ts
+++ src/ocean/Assets.ts
@@ -756,7 +756,7 @@ export class Assets extends Instantiable {
    * @param {consumer} string
    * @return {Promise<Consumable>}
    */
-  public async isConsumable(ddo: DDO, consumer?: string, credentialType?: CredentialType): Promise<Consumable> {
+  public async isConsumable(ddo: DDO, consumer?: string, credentialType: CredentialType = CredentialType.address): Promise<Consumable> {
     let status = 0
     let message = 'All good'
     let result = true
@@ -770,19 +770,11 @@ export class Assets extends Instantiable {
         result: false
       }
     if (consumer) {
-      if (credentialType && credentialType == CredentialType.domain){
         ;({ status, message, result } = this.checkCredential(
           ddo,
-          CredentialType.domain,
+          credentialType,
           consumer
         ))
-      }else{
-        ;({ status, message, result } = this.checkCredential(
-          ddo,
-          CredentialType.address,
-          consumer
-        ))
-      }
     }
     /*
     // return: 2, Access is denied, your wallet address is not found on allow list
