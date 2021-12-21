4c1be93adca09abdf493961b14cdff1172e57c29
mihaisc <mihai.scarlat@smartcontrol.ro>
2021-12-21 21:50:34 +0200

    ensure chainId is base 10
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- src/utils/DdoHelpers.ts
+++ src/utils/DdoHelpers.ts
@@ -3,7 +3,7 @@ import Web3 from 'web3'
 
 export function generateDid(erc721Address: string, chainId: number): string {
   erc721Address = Web3.utils.toChecksumAddress(erc721Address)
-  const checksum = sha256(erc721Address + chainId)
+  const checksum = sha256(erc721Address + chainId.toString(10))
   return `did:op:${checksum.toString()}`
 }
 
