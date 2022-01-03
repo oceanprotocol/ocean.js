833e02e15610210e07aafff88a0776793ffa3590
alexcos20 <alex.coseru@gmail.com>
2022-01-03 03:33:01 -0800

    fix integration

--- test/integration/PublishNFTErc20.test.ts
+++ test/integration/PublishNFTErc20.test.ts
@@ -30,6 +30,7 @@ const ddo = {
   id: 'did:op:efba17455c127a885ec7830d687a8f6e64f5ba559f8506f8723c1f10f05c049c',
   version: '4.0.0',
   chainId: 4,
+  nftAddress: '0x0',
   metadata: {
     created: '2021-12-20T14:35:20Z',
     updated: '2021-12-20T14:35:20Z',
