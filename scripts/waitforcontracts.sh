0721b2260df00ebd7becd811b875475dbcf538c9
alexcos20 <alex.coseru@gmail.com>
2022-02-01 04:20:22 -0800

    fix

--- scripts/waitforcontracts.sh
+++ scripts/waitforcontracts.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] ; do
+while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] || [ ! -f "$HOME/.ocean/ocean-c2d/ready" ]; do
   sleep 2
 done
-docker logs ocean_kindcluster_1 --follow &
-docker logs ocean_computetodata_1 --follow &
-
-while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] || [ ! -f "$HOME/.ocean/ocean/c2d/ready" ]; do
-  find $HOME/.ocean
-  sleep 2
-done
-cat "barge/start_ocean.log"
+#cat "barge/start_ocean.log"
 ls -lh "${HOME}/.ocean/ocean-contracts/"
 ls -lh "${HOME}/.ocean/ocean-contracts/artifacts/"
 cat "${HOME}/.ocean/ocean-contracts/artifacts/address.json"
