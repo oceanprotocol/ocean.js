34689e4fd7715c5d1f1a17deecb68f470b21c314
alexcos20 <alex.coseru@gmail.com>
2022-02-01 03:49:59 -0800

    more debug

--- scripts/waitforcontracts.sh
+++ scripts/waitforcontracts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] || [ ! -f "$HOME/.ocean/ocean/c2d/ready" ]; do
+  echo "Waiting for barge to spin up..."
   [ -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] && echo "Contracts ready.."
   [ -f "$HOME.ocean/ocean/c2d/ready" ] && echo "C2d ready.."
   sleep 2
