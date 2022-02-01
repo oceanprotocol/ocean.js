f87a7ef476763b0c82e03a22fd40116524773e26
alexcos20 <alex.coseru@gmail.com>
2022-02-01 04:12:38 -0800

    more debug

--- scripts/waitforcontracts.sh
+++ scripts/waitforcontracts.sh
@@ -6,6 +6,7 @@ docker logs ocean_kindcluster_1 --follow &
 docker logs ocean_computetodata_1 --follow &
 
 while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] || [ ! -f "$HOME/.ocean/ocean/c2d/ready" ]; do
+  find $HOME/.ocean
   sleep 2
 done
 cat "barge/start_ocean.log"
