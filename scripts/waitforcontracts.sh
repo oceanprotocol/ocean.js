c9d0a5be564f33f4ab56d81071032141abfbd892
alexcos20 <alex.coseru@gmail.com>
2022-02-01 03:48:09 -0800

    small test

--- scripts/waitforcontracts.sh
+++ scripts/waitforcontracts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] || [ ! -f "$HOME/.ocean/ocean/c2d/ready" ]; do
+  [ -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ] && echo "Contracts ready.."
+  [ -f "$HOME.ocean/ocean/c2d/ready" ] && echo "C2d ready.."
   sleep 2
 done
 cat "barge/start_ocean.log"
