fb2ba5dade2181ad4c6131f1333d92e42ceaa146
alexcos20 <alex.coseru@gmail.com>
2022-02-09 06:07:13 -0800

    test check

--- test/integration/ComputeFlow.test.ts
+++ test/integration/ComputeFlow.test.ts
@@ -247,6 +247,8 @@ describe('Simple compute tests', async () => {
 
     // get compute environments
     const computeEnvs = await ProviderInstance.getComputeEnvironments(providerUrl)
+    console.log(computeEnvs)
+    assert(computeEnvs && computeEnvs.length>0)
     // we choose the first env
     const computeEnv = computeEnvs[0].id
     const computeConsumerAddress = computeEnvs[0].consumerAddress
