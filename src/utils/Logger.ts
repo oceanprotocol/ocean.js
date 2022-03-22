0c7f9e292e3942c46407945950ededed57db9093
Matthias Kretschmann <m@kretschmann.io>
2022-03-22 15:06:27 +0000

    solve all typing warnings

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 export enum LogLevel {
   None = -1,
   Error = 0,
@@ -5,6 +6,7 @@ export enum LogLevel {
   Log = 2,
   Verbose = 3
 }
+/* eslint-enable no-unused-vars */
 
 export class Logger {
   constructor(private logLevel: LogLevel = LogLevel.Error) {}
