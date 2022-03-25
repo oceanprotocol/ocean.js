17ee00cf9e4644f9df0d66c2b0d76e37fc34850b
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2022-03-25 15:34:14 +0200

    update sha256 import from cryptojs lib

--- src/utils/DdoHelpers.ts
+++ src/utils/DdoHelpers.ts
@@ -1,16 +1,16 @@
-import CryptoJS from 'crypto-js'
+import sha256 from 'crypto-js/sha256'
 import Web3 from 'web3'
 import LoggerInstance from './Logger'
 
 export function generateDid(erc721Address: string, chainId: number): string {
   erc721Address = Web3.utils.toChecksumAddress(erc721Address)
-  const checksum = CryptoJS.SHA256(erc721Address + chainId.toString(10))
+  const checksum = sha256(erc721Address + chainId.toString(10))
   return `did:op:${checksum.toString()}`
 }
 
 export function getHash(data: any): string {
   try {
-    return CryptoJS.SHA256(data).toString()
+    return sha256(data).toString()
   } catch (e) {
     LoggerInstance.error('getHash error: ', e.message)
   }
