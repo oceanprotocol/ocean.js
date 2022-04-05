23101a185fb4f885a8c699b2c8795278f1169dd9
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-04-05 14:53:04 +0200

    add generic estimateGas() function

--- src/utils/ContractUtils.ts
+++ src/utils/ContractUtils.ts
@@ -11,6 +11,7 @@ import {
 import { Config } from '../models'
 import { minAbi } from './minAbi'
 import LoggerInstance from './Logger'
+import { GASLIMIT_DEFAULT } from './Constants'
 
 export function setContractDefaults(contract: Contract, config: Config): Contract {
   if (config) {
@@ -161,4 +162,30 @@ export async function amountToUnits(
   } catch (e) {
     LoggerInstance.error(`ERROR: FAILED TO CALL DECIMALS(), USING 18', ${e.message}`)
   }
+}
+
+/**
+ * Estimates the gas used when a function would be executed on chain
+ * @param {string} from account that calls the function
+ * @param {Function} functionToEstimateGas function that we need to estimate the gas
+ * @param {...any[]} args arguments of the function
+ * @return {Promise<number>} gas cost of the function
+ */
+export async function estimateGas(
+  from: string,
+  functionToEstimateGas: Function,
+  ...args: any[]
+): Promise<number> {
+  let estimatedGas = GASLIMIT_DEFAULT
+  try {
+    estimatedGas = await functionToEstimateGas.apply(null, args).estimateGas(
+      {
+        from: from
+      },
+      (err, estGas) => (err ? GASLIMIT_DEFAULT : estGas)
+    )
+  } catch (e) {
+    LoggerInstance.error(`ERROR: Estimate gas failed!`, e)
+  }
+  return estimatedGas
 }
