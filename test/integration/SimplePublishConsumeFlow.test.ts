868de7c17863e90404e165819d2d73983765f5ed
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-05-18 11:47:02 +0300

    fixing linting & solving conflict

--- test/integration/SimplePublishConsumeFlow.test.ts
+++ test/integration/SimplePublishConsumeFlow.test.ts
@@ -14,47 +14,6 @@ import {
   ZERO_ADDRESS
 } from '../../src'
 import { ProviderFees, Erc20CreateParams, DDO } from '../../src/@types'
-<<<<<<< HEAD
-
-const assetUrl = [
-  {
-    type: 'url',
-    url: 'https://raw.githubusercontent.com/oceanprotocol/testdatasets/main/shs_dataset_test.txt',
-    method: 'GET'
-  }
-]
-const ddo: DDO = {
-  '@context': ['https://w3id.org/did/v1'],
-  id: 'did:op:efba17455c127a885ec7830d687a8f6e64f5ba559f8506f8723c1f10f05c049c',
-  version: '4.0.0',
-  chainId: 4,
-  nftAddress: '0x0',
-  metadata: {
-    created: '2021-12-20T14:35:20Z',
-    updated: '2021-12-20T14:35:20Z',
-    type: 'dataset',
-    name: 'dfgdfgdg',
-    description: 'd dfgd fgd dfg dfgdfgd dfgdf',
-    tags: [''],
-    author: 'dd',
-    license: 'https://market.oceanprotocol.com/terms',
-    additionalInformation: {
-      termsAndConditions: true
-    }
-  },
-  services: [
-    {
-      id: 'notAnId',
-      type: 'access',
-      files: '',
-      datatokenAddress: '0xa15024b732A8f2146423D14209eFd074e61964F3',
-      serviceEndpoint: 'https://providerv4.rinkeby.oceanprotocol.com',
-      timeout: 0
-    }
-  ]
-}
-=======
->>>>>>> 620b5d3a24e3d76a7b9ac1fc78a370add76f549a
 
 describe('Simple Publish & consume test', async () => {
   let config: Config
