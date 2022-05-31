ca1926d0fcb15fd8ea78c6f2079dffd823849c24
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-05-31 12:02:50 +0200

    remove unused functions from FetchHelper

--- src/utils/FetchHelper.ts
+++ src/utils/FetchHelper.ts
@@ -1,16 +1,5 @@
 import fetch from 'cross-fetch'
-import { LoggerInstance } from '.'
 import { DownloadResponse } from '../@types'
-
-export async function fetchData(url: string, opts: RequestInit): Promise<Response> {
-  const result = await fetch(url, opts)
-  if (!result.ok) {
-    LoggerInstance.error(`Error requesting [${opts.method}] ${url}`)
-    LoggerInstance.error(`Response message: \n${await result.text()}`)
-    throw result
-  }
-  return result
-}
 
 export async function downloadFileBrowser(url: string): Promise<void> {
   const anchor = document.createElement('a')
@@ -41,29 +30,4 @@ export async function downloadFile(
   }
 
   return { data: await response.arrayBuffer(), filename }
-}
-
-async function postWithHeaders(
-  url: string,
-  payload: BodyInit,
-  headers: any
-): Promise<Response> {
-  if (payload != null) {
-    return fetch(url, {
-      method: 'POST',
-      body: payload,
-      headers
-    })
-  } else {
-    return fetch(url, {
-      method: 'POST'
-    })
-  }
-}
-
-export async function postData(url: string, payload: BodyInit): Promise<Response> {
-  const headers = {
-    'Content-type': 'application/json'
-  }
-  return postWithHeaders(url, payload, headers)
 }
