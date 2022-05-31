3a4da10c08595336b4a13f6d5775429d52cd1c3f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-05-31 18:06:35 +0100

    comparing the checksum of the CodeExamples.md file to see if it has been changed before commiting

--- scripts/commitChanges.sh
+++ scripts/commitChanges.sh
@@ -1,14 +1,12 @@
 # Check if the CodeExamples.md file has been changed
-FILE=CodeExamples.md
-# Timeframe for the comparison
-OLDTIME=60
-# Get current and file times
-CURTIME=$(date +%s)
-FILETIME=$(stat $FILE -c %Y)
-TIMEDIFF=$(expr $CURTIME - $FILETIME)
+original=$(md5sum CodeExamples.md)
+echo $original
+npm run create:guide
+new=$(md5sum CodeExamples.md)
+echo $new
 
-# Check if file older
-if [ $TIMEDIFF -gt $OLDTIME ]; then
+# Check if file has changed
+if [ "$new" = "$original" ]; then
     echo "CodeExamples.md file has not been changed"
 else
     echo "CodeExamples.md file has been changed. Committing changes"
