39b11179d3140d6303d3b0b3fcfc71ed3d3d7ab0
alexcos20 <alex.coseru@gmail.com>
2022-06-13 22:31:57 +0300

    fix computeflow

--- test/integration/ComputeFlow.test.ts
+++ test/integration/ComputeFlow.test.ts
@@ -297,7 +297,8 @@ async function handleOrder(
      - no validOrder -> we need to call startOrder, to pay 1 DT & providerFees
   */
   if (order.providerFee && order.providerFee.providerFeeAmount) {
-    await datatoken.approve(
+    order.providerFee.providerFeeAmount = String(order.providerFee.providerFeeAmount)
+    await datatoken.approveWei(
       order.providerFee.providerFeeToken,
       datatokenAddress,
       order.providerFee.providerFeeAmount,
