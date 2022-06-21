3a1abb2813e569b54de9edf89cf40890df1665c5
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-21 11:29:05 +0200

    Remove try...catch from ConfigHelper

--- src/config/ConfigHelper.ts
+++ src/config/ConfigHelper.ts
@@ -148,33 +148,28 @@ export class ConfigHelper {
 
     // load from custom addresses structure
     if (customAddresses) {
-      try {
-        const {
-          FixedPrice,
-          Dispenser,
-          Staking,
-          poolTemplate,
-          ERC721Factory,
-          OPFCommunityFeeCollector,
-          Ocean,
-          chainId,
-          startBlock
-        } = customAddresses[network]
-        configAddresses = {
-          nftFactoryAddress: ERC721Factory,
-          sideStakingAddress: Staking,
-          opfCommunityFeeCollector: OPFCommunityFeeCollector,
-          poolTemplateAddress: poolTemplate,
-          fixedRateExchangeAddress: FixedPrice,
-          dispenserAddress: Dispenser,
-          oceanTokenAddress: Ocean,
-          chainId: chainId,
-          startBlock: startBlock,
-          ...(process.env.AQUARIUS_URI && { metadataCacheUri: process.env.AQUARIUS_URI })
-        }
-      } catch (e) {
-        // console.error(`ERROR: Could not load local contract address file: ${e.message}`)
-        // return null
+      const {
+        FixedPrice,
+        Dispenser,
+        Staking,
+        poolTemplate,
+        ERC721Factory,
+        OPFCommunityFeeCollector,
+        Ocean,
+        chainId,
+        startBlock
+      } = customAddresses[network]
+      configAddresses = {
+        nftFactoryAddress: ERC721Factory,
+        sideStakingAddress: Staking,
+        opfCommunityFeeCollector: OPFCommunityFeeCollector,
+        poolTemplateAddress: poolTemplate,
+        fixedRateExchangeAddress: FixedPrice,
+        dispenserAddress: Dispenser,
+        oceanTokenAddress: Ocean,
+        chainId: chainId,
+        startBlock: startBlock,
+        ...(process.env.AQUARIUS_URI && { metadataCacheUri: process.env.AQUARIUS_URI })
       }
     } else {
       // no custom addresses structure was passed, trying to load default
