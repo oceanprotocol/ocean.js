e18c1304378166e7340d974833c8f7ce18244504
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2022-09-23 15:51:50 +0300

    wip

--- scripts/commitChanges.sh
+++ scripts/commitChanges.sh
@@ -18,3 +18,24 @@ else
     echo Pushing changes to branch: ${branch}
     git push origin HEAD:${branch} --force
 fi
+
+# Check if the C2DExamples.md file has been changed
+original=$(md5sum C2DExamples.md)
+echo $original
+npm run create:guidec2d
+new=$(md5sum C2DExamples.md)
+echo $new
+
+# Check if file has changed
+if [ "$new" = "$original" ]; then
+    echo "C2DExamples.md file has not been changed"
+else
+    echo "C2DExamples.md file has been changed. Committing changes"
+    # Stage the file, commit and push
+    git status
+    git add C2DExamples.md
+    git commit -m "Updating C2DExamples.md"
+    branch=${GITHUB_HEAD_REF#refs/heads/}
+    echo Pushing changes to branch: ${branch}
+    git push origin HEAD:${branch} --force
+fi
