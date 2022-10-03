29aa4854772d65e4410ffade97387d3dd63cb573
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-10-03 11:14:52 -0400

    Update Provider tests to use UrlFile argument

--- test/integration/Provider.test.ts
+++ test/integration/Provider.test.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai'
 import { web3, getTestConfig } from '../config'
 import { Config, Provider } from '../../src'
-import { FileInfo } from '../../src/@types'
+import { FileInfo, UrlFile } from '../../src/@types'
 
 describe('Provider tests', async () => {
   let config: Config
@@ -27,7 +27,11 @@ describe('Provider tests', async () => {
 
   it('Alice checks fileinfo', async () => {
     const fileinfo: FileInfo[] = await providerInstance.checkFileUrl(
-      'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz-rss.xml',
+      {
+        'type': 'url',
+        'url': 'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz-rss.xml',
+        'method': 'GET',
+      },
       config.providerUri
     )
     assert(fileinfo[0].valid === true, 'Sent file is not valid')
