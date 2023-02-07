5306d752085e2c2cc45bccea09808dfcf51796aa
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-02-07 23:27:40 +0200

    Set withMint default to true (#1693)
    * make withMint default to true
    
    * fix lint

--- src/@types/Dispenser.ts
+++ src/@types/Dispenser.ts
@@ -2,7 +2,7 @@ export interface DispenserCreationParams {
   dispenserAddress: string
   maxTokens: string // how many tokens cand be dispensed when someone requests . If maxTokens=2 then someone can't request 3 in one tx
   maxBalance: string // how many dt the user has in it's wallet before the dispenser will not dispense dt
-  withMint?: boolean // true if we want to allow the dispenser to be a minter
+  withMint?: boolean // true if we want to allow the dispenser to be a minter, default true
   allowedSwapper?: string // only account that can ask tokens. set address(0) if not required
 }
 
