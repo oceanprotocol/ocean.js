fbcd13ac29d66bdc818e0c54ffcd606204a4e008
Alex Coseru <alex.coseru@gmail.com>
2023-04-13 23:29:04 +0300

    Make FRE withMint = True (#1719)
    * Updating CodeExamples.md

--- test/integration/CodeExamples.test.ts
+++ test/integration/CodeExamples.test.ts
@@ -307,7 +307,7 @@ describe('Marketplace flow tests', async () => {
       fixedRate: '1',
       marketFee: '0.001',
       allowedConsumer: ZERO_ADDRESS,
-      withMint: false
+      withMint: true
     }
 
     const tx = await factory.createNftWithDatatokenWithFixedRate(
