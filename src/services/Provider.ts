5d15652d5c60092eaa9251f8706f39bab9cc5387
mariacarmina <mariacarmina.cretu@gmail.com>
2024-06-21 17:46:22 +0300

    Fix lint.

--- src/services/Provider.ts
+++ src/services/Provider.ts
@@ -61,7 +61,9 @@ export class Provider {
       const endpoint: ServiceEndpoint = {
         serviceName: i,
         method: endpoints.serviceEndpoints[i][0],
-        urlPath: providerEndpoint.endsWith('/') ? providerEndpoint + endpoints.serviceEndpoints[i][1] : providerEndpoint + '/' + endpoints.serviceEndpoints[i][1]
+        urlPath: providerEndpoint.endsWith('/')
+          ? providerEndpoint + endpoints.serviceEndpoints[i][1]
+          : providerEndpoint + '/' + endpoints.serviceEndpoints[i][1]
       }
       serviceEndpoints.push(endpoint)
     }
