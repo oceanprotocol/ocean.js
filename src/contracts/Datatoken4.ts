e59d565b176294e5808bdeddea9c1abae798bdbd
mariacarmina <mariacarmina.cretu@gmail.com>
2024-09-02 15:45:47 +0300

    added sapphire sdk + remove private functions.

--- src/contracts/Datatoken4.ts
+++ src/contracts/Datatoken4.ts
@@ -152,64 +152,6 @@ export class Datatoken4 extends Datatoken {
     return <ReceiptOrEstimate<G>>trxReceipt
   }
 
-  /** addConsumer
-   * This function allows to add consumer
-   * only DatatokenDeployer can succeed
-   * @param {String} dtAddress Datatoken address
-   * @param {String} consumer User address
-   * @param {Boolean} estimateGas if True, return gas estimate
-   * @return {Promise<ReceiptOrEstimate>} transactionId
-   */
-  public async addConsumer<G extends boolean = false>(
-    dtAddress: string,
-    consumer: string,
-    estimateGas?: G
-  ): Promise<ReceiptOrEstimate<G>> {
-    const dtContract = this.getContract(dtAddress)
-    const estGas = await dtContract.estimateGas.addConsumer(consumer)
-    if (estimateGas) return <ReceiptOrEstimate<G>>estGas
-
-    const trxReceipt = await sendTx(
-      estGas,
-      this.signer,
-      this.config?.gasFeeMultiplier,
-      dtContract.addConsumer,
-      consumer
-    )
-
-    return <ReceiptOrEstimate<G>>trxReceipt
-  }
-
-  /** checkConsumer
-   * This function allows to add consumer
-   * only DatatokenDeployer can succeed
-   * @param {String} dtAddress Datatoken address
-   * @param {Number} serviceId Service Identifier
-   * @param {String} consumer User address
-   * @param {Boolean} estimateGas if True, return gas estimate
-   * @return {Promise<ReceiptOrEstimate>} transactionId
-   */
-  public async checkConsumer<G extends boolean = false>(
-    dtAddress: string,
-    serviceId: number,
-    consumer: string,
-    estimateGas?: G
-  ): Promise<ReceiptOrEstimate<G>> {
-    const dtContract = this.getContract(dtAddress)
-    const estGas = await dtContract.estimateGas.checkConsumer(serviceId, consumer)
-    if (estimateGas) return <ReceiptOrEstimate<G>>estGas
-
-    const trxReceipt = await sendTx(
-      estGas,
-      this.signer,
-      this.config?.gasFeeMultiplier,
-      dtContract.checkConsumer,
-      serviceId,
-      consumer
-    )
-    return <ReceiptOrEstimate<G>>trxReceipt
-  }
-
   /**
    * getFileObject - It returns the consumer's file object encrypted format.
    * @param {String} dtAddress datatoken address
