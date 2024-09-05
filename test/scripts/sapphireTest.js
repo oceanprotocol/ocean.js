59bdf3d60872511127393d16260fc63d258bb3cf
mariacarmina <mariacarmina.cretu@gmail.com>
2024-09-05 16:33:52 +0300

    Install tsx, added oasis testnet into config helpers.

--- test/scripts/sapphireTest.js
+++ test/scripts/sapphireTest.js
@@ -1,10 +1,9 @@
 import * as sapphire from '@oasisprotocol/sapphire-paratime'
+import addresses from '@oceanprotocol/contracts/addresses/address.json'
 import { ethers, Signer } from 'ethers'
-import { getAddressesForSapphire } from '../config'
-import { AccesslistFactory } from '../../src/contracts/AccessListFactory'
-import { AccessListContract } from '../../src/contracts/AccessList'
-import { AccessListData } from '../../src/@types'
-import { ZERO_ADDRESS } from '../../src'
+import { AccesslistFactory } from '../../src/contracts/AccessListFactory.ts'
+import { AccessListContract } from '../../src/contracts/AccessList.ts'
+import { ZERO_ADDRESS } from '../../src/utils/Constants.ts'
 import { assert } from 'console'
 
 const provider = sapphire.wrap(
@@ -12,7 +11,8 @@ const provider = sapphire.wrap(
 )
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
-const addresses = await getAddressesForSapphire(true)
+const addrs = addresses.oasis_saphire_testnet
+console.log(`addrs: ${JSON.stringify(addrs)}`)
 const listData = {
   name: 'ListName',
   symbol: 'ListSymbol',
@@ -22,7 +22,11 @@ const listData = {
   user: [await wallet.getAddress(), ZERO_ADDRESS]
 }
 
-const factoryContract = new AccesslistFactory(addresses.AccessListFactory, wallet, 23295)
+const factoryContract = new AccesslistFactory(
+  addrs.AccessListFactory,
+  wallet,
+  addrs.chainId
+)
 assert(factoryContract !== null, 'factory not created')
 
 listData.owner = await wallet.getAddress()
