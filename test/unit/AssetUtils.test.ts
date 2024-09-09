5611eab8176d9eab9c86a7f38eb23a3e893930e3
paulo-ocean <paulo@oceanprotocol.com>
2024-09-09 12:48:06 +0100

    remove hardhat deps

--- test/unit/AssetUtils.test.ts
+++ test/unit/AssetUtils.test.ts
@@ -1,8 +1,21 @@
 import { assert } from 'chai'
 import { KNOWN_CONFIDENTIAL_EVMS } from '../../src/config'
-import { calculateTemplateIndex, isConfidentialEVM } from '../../src/utils'
+import { provider, getAddresses } from '../config'
+import {
+  calculateActiveTemplateIndex,
+  calculateTemplateIndex,
+  isConfidentialEVM
+} from '../../src/utils'
+import { Signer } from 'ethers/lib/ethers'
 
+let nftOwner: Signer
+let addresses: any
 describe('Asset utils (createAsset)', () => {
+  before(async () => {
+    nftOwner = (await provider.getSigner(0)) as Signer
+    addresses = await getAddresses()
+  })
+
   it('should check if confidential EVM', async () => {
     for (const network of KNOWN_CONFIDENTIAL_EVMS.networks) {
       network.name.map((networkName) => {
@@ -78,5 +91,22 @@ describe('Asset utils (createAsset)', () => {
       3 // wrong template
     )
     assert(notOkIndexNonConfidential === -1, 'Template 3 should not exist on sepolia!')
+  })
+
+  // checking if active by connecting to the smart contract as well
+  it('V2 - should get correct template index from contract artifacts (using template ID as template)', async () => {
+    const okTemplate = await calculateActiveTemplateIndex(
+      nftOwner,
+      addresses.ERC721Factory,
+      3
+    )
+    assert(okTemplate === 3, 'wrong template index, should be index 3!')
+
+    const wrongOne = await calculateActiveTemplateIndex(
+      nftOwner,
+      addresses.ERC721Factory,
+      6
+    )
+    assert(wrongOne === -1, 'wrong template index, should only exist 3!')
   })
 })
