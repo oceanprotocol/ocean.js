d5fbd1a183a03c746472b06e12eb30f49f3e44bb
paulo-ocean <paulo@oceanprotocol.com>
2025-01-20 15:30:11 +0000

    wip: updating the compute env spces

--- src/@types/Compute.ts
+++ src/@types/Compute.ts
@@ -6,16 +6,58 @@ export type ComputeResultType =
   | 'configrationLog'
   | 'publishLog'
 
+// OLD V1 ComputeEnvironment specs
+// export interface ComputeEnvironment {
+//   id: string
+//   cpuNumber: number
+//   cpuType: string
+//   gpuNumber: number
+//   gpuType: string
+//   ramGB: number
+//   diskGB: number
+//   priceMin: number
+//   desc: string
+//   currentJobs: number
+//   maxJobs: number
+//   consumerAddress: string
+//   storageExpiry: number
+//   maxJobDuration: number
+//   lastSeen: number
+//   free: boolean
+// }
+
+// new V2 C2D Compute Environment specs
+
+export type ComputeResourceType = 'cpu' | 'memory' | 'storage'
+
+export interface ComputeResourcesPricingInfo {
+  type: ComputeResourceType
+  price: number
+}
+export interface ComputeEnvFees {
+  feeToken: string
+  prices: ComputeResourcesPricingInfo[]
+}
+export interface ComputeEnvFeesStructure {
+  [chainId: string]: ComputeEnvFees
+}
 export interface ComputeEnvironment {
   id: string
-  cpuNumber: number
-  cpuType: string
-  gpuNumber: number
-  gpuType: string
-  ramGB: number
-  diskGB: number
-  priceMin: number
-  desc: string
+  // cpuNumber: number
+  // cpuType: string
+  // gpuNumber: number
+  // gpuType: string
+  // ramGB: number
+  // diskGB: number
+  // priceMin: number
+  totalCpu: number // total cpu available for jobs
+  maxCpu: number // max cpu for a single job.  Imagine a K8 cluster with two nodes, each node with 10 cpus.  Total=20, but at most you can allocate 10 cpu for a job
+  totalRam: number // total gb of RAM
+  maxRam: number // max allocatable GB RAM for a single job.
+  maxDisk: number // max GB of disck allocatable for a single job
+  fees: ComputeEnvFeesStructure
+  // legacy
+  description: string
   currentJobs: number
   maxJobs: number
   consumerAddress: string
