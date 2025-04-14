7ada9fd5a630c9317d9520f5433c2028a84da692
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2025-04-14 10:52:56 +0300

    add json file

--- test/integration/PublishEditConsume.test.ts
+++ test/integration/PublishEditConsume.test.ts
@@ -144,7 +144,7 @@ function uploadToIpfs(data: any): Promise<string> {
           'multipart/form-data; boundary=------------------------a28d68b1c872c96f'
       },
       body:
-        '--------------------------a28d68b1c872c96f\r\nContent-Disposition: form-data; name="file"; filename="ddo.json"\r\nContent-Type: application/octet-stream\r\n\r\n' +
+        '--------------------------a28d68b1c872c96f\r\nContent-Disposition: form-data; name="file"; filename="./resources/data.json"\r\nContent-Type: application/octet-stream\r\n\r\n' +
         data +
         '\r\n--------------------------a28d68b1c872c96f--\r\n'
     })
