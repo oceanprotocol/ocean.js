eba976d7bd360f5765b9a56dbdc26f8606fd166a
mariacarmina <mariacarmina.cretu@gmail.com>
2025-04-18 21:25:25 +0300

    logs for config.

--- src/config/ConfigHelper.ts
+++ src/config/ConfigHelper.ts
@@ -277,6 +277,11 @@ export class ConfigHelper {
     const filterBy = typeof network === 'string' ? 'network' : 'chainId'
 
     let config = configHelperNetworks.find((c) => c[filterBy] === network)
+    LoggerInstance.log(
+      `config: ${JSON.stringify(config)}, configHelperNetworks: ${JSON.stringify(
+        configHelperNetworks
+      )}`
+    )
 
     if (!config) {
       LoggerInstance.error(`No config found for given network '${network}'`)
